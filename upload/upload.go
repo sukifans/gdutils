aa9b38d5008fe62c1f5ca984bf36477908350c53
Lunre <1500868816@qq.com>
2021-09-21 01:20:57 +0800

    提交mod

---
+++ upload/upload.go
@@ -0,0 +1,193 @@
+package upload
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type ServerClient struct {
+	Server *drive.Service
+
+	FoldType string
+}
+
+func NewServerClient(Server *drive.Service) ServerClient {
+
+	ServerClient := ServerClient{
+		Server:   Server,
+		FoldType: "application/vnd.google-apps.folder",
+	}
+
+	return ServerClient
+
+}
+
+func GetConfig(CredentialsPath string) *oauth2.Config {
+
+	b, err := ioutil.ReadFile(CredentialsPath)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token-old.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	return config
+
+}
+
+//从文件中获取Token
+func GetTokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+//网络请求token
+// Request a token from the web, then returns the retrieved token.
+func GetTokenFromWeb(config *oauth2.Config, ProcessAuthURl func(AuthUrl string), ReturnAuthCode func() string) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+
+	//此处为输出验证文件回调函数
+	ProcessAuthURl(authURL)
+
+	var authCode string
+
+	//此处为获取信息回调函数
+	authCode = ReturnAuthCode()
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+
+	return tok
+}
+
+func SaveToken(path string, token *oauth2.Token) {
+
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+
+}
+
+//获取客户端
+func GetClient(config *oauth2.Config, tok *oauth2.Token) *http.Client {
+
+	return config.Client(context.Background(), tok)
+
+}
+
+//获取服务器
+func GetService(client *http.Client) *drive.Service {
+
+	ctx := context.Background()
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	return srv
+
+}
+
+//获取为共享drive的id
+//不包含自己的drive
+
+func (s *ServerClient) GetDriveList(PageSize int64) (*drive.DriveList, error) {
+
+	List, err := s.Server.Drives.List().PageSize(PageSize).Do()
+
+	return List, err
+
+}
+
+//获取文件列表
+func (s *ServerClient) GetFiles(DriveId string) ([]*drive.File, error) {
+
+	FileList, err := s.Server.Files.List().Corpora("drive").IncludeItemsFromAllDrives(true).SupportsAllDrives(true).DriveId(DriveId).Do()
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	return FileList.Files, err
+
+}
+
+//获取文件夹列表
+func (s *ServerClient) GetFolders(DriveId string) ([]*drive.File, error) {
+
+	FileList, err := s.Server.Files.List().Corpora("drive").Q("mimeType='application/vnd.google-apps.folder'").OrderBy("createdTime desc").IncludeItemsFromAllDrives(true).SupportsAllDrives(true).DriveId(DriveId).Do()
+
+	var FolderList []*drive.File
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	FolderList = FileList.Files
+
+	return FolderList, err
+
+}
+
+//上传文件
+func (s *ServerClient) Upload(FileName string, FolderId string, Reader io.Reader) (*drive.File, error) {
+
+	FileMeta := &drive.File{Name: FileName, Parents: []string{FolderId}}
+
+	FileInfo, err := s.Server.Files.Create(FileMeta).Media(Reader).SupportsAllDrives(true).Do()
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	return FileInfo, err
+
+}
+
+func (s ServerClient) CreateFolder(FolderName string, DriveId string) (*drive.File, error) {
+
+	FileMeta := &drive.File{Name: FolderName, DriveId: DriveId, Parents: []string{DriveId}, MimeType: s.FoldType}
+
+	FolderInfo, err := s.Server.Files.Create(FileMeta).SupportsAllDrives(true).SupportsTeamDrives(true).Do()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return FolderInfo, err
+
+}
