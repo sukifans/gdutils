f8cefd5fec8270236487029ca9976b3ce7d418e8
Mmx233 <admin@mmxblog.com>
2022-02-26 20:01:17 +0800

    feat: 定义token别名

--- upload.go
+++ upload.go
@@ -14,6 +14,21 @@ import (
 	"os"
 )
 
+type Token oauth2.Token
+
+func (a *Token) SaveTo(path string) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	err = json.NewEncoder(f).Encode(a)
+	if err != nil {
+		log.Fatalf("Unable to decode oauth token: %v", err)
+	}
+}
+
 type ServerClient struct {
 	Server   *drive.Service
 	FoldType string
@@ -39,13 +54,13 @@ func GetConfig(Credentials []byte) *oauth2.Config {
 }
 
 // GetTokenFromBytes 从bytes获取Token
-func GetTokenFromBytes(data []byte) (c *oauth2.Token, e error) {
+func GetTokenFromBytes(data []byte) (c *Token, e error) {
 	e = json.Unmarshal(data, c)
 	return
 }
 
 // GetTokenFromFile 从文件中获取Token
-func GetTokenFromFile(file string) (*oauth2.Token, error) {
+func GetTokenFromFile(file string) (*Token, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
@@ -53,21 +68,16 @@ func GetTokenFromFile(file string) (*oauth2.Token, error) {
 	defer f.Close()
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
+	return (*Token)(tok), err
 }
 
 // GetTokenFromWeb 网络请求token
 // Request a token from the web, then returns the retrieved token.
-func GetTokenFromWeb(config *oauth2.Config, ProcessAuthURl func(AuthUrl string), ReturnAuthCode func() string) *oauth2.Token {
+func GetTokenFromWeb(config *oauth2.Config, ProcessAuthURl func(AuthUrl string) (AuthCode string)) *Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 
-	//此处为输出验证文件回调函数
-	ProcessAuthURl(authURL)
-
-	var authCode string
-
-	//此处为获取信息回调函数
-	authCode = ReturnAuthCode()
+	//此处为输出验证文件回调函数与获取信息回调函数
+	authCode := ProcessAuthURl(authURL)
 
 	tok, err := config.Exchange(context.TODO(), authCode)
 
@@ -75,30 +85,17 @@ func GetTokenFromWeb(config *oauth2.Config, ProcessAuthURl func(AuthUrl string),
 		log.Fatalf("Unable to retrieve token from web %v", err)
 	}
 
-	return tok
-}
-
-func SaveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	err = json.NewEncoder(f).Encode(token)
-	if err != nil {
-		log.Fatalf("Unable to decode oauth token: %v", err)
-	}
+	return (*Token)(tok)
 }
 
 // getClient 获取客户端
-func getClient(config *oauth2.Config, tok *oauth2.Token) *http.Client {
-	return config.Client(context.Background(), tok)
+func getClient(config *oauth2.Config, tok *Token) *http.Client {
+	return config.Client(context.Background(), (*oauth2.Token)(tok))
 }
 
 // GetService 获取服务器
 // 进一步封装,优化结构
-func GetService(config *oauth2.Config, tok *oauth2.Token) *drive.Service {
+func GetService(config *oauth2.Config, tok *Token) *drive.Service {
 	srv, err := drive.NewService(context.Background(), option.WithHTTPClient(getClient(config, tok)))
 
 	if err != nil {
